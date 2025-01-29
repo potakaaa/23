93ed76b6c40917cef640ed6281612919ce681e2b
potakaaa <helbirog@gmail.com>
2025-01-29 10:09:03 +0800

    Initialized files

--- app/components/client/ImagePuzzle.tsx
+++ app/components/client/ImagePuzzle.tsx
@@ -3,14 +3,12 @@
 import { JigsawPuzzle } from "react-jigsaw-puzzle/lib";
 import "react-jigsaw-puzzle/lib/jigsaw-puzzle.css";
 import "./puzzle.css";
-
-const IMAGE_URL =
-  "https://scontent.fcgm1-1.fna.fbcdn.net/v/t39.30808-6/475384339_3987666824891550_5527229469945314392_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Edf0tw3tUOQQ7kNvgGorff-&_nc_zt=23&_nc_ht=scontent.fcgm1-1.fna&_nc_gid=A65jpWaxXpSY3_ZDxNqxB7q&oh=00_AYAoKRdiOoowu5t-NnJ6vXrmIUj6qxwcoQedYSBivaTi3w&oe=679F708C";
+import { imageSrc } from "@/app/provider/global";
 
 const ImagePuzzle = () => {
   return (
     <JigsawPuzzle
-      imageSrc={IMAGE_URL}
+      imageSrc={imageSrc}
       rows={3}
       columns={4}
       onSolved={() => alert("Solved!")}
--- app/puzzle/page.tsx
+++ app/puzzle/page.tsx
@@ -3,8 +3,19 @@ import ImagePuzzle from "../components/client/ImagePuzzle";
 
 const PuzzlePage = () => {
   return (
-    <div className="h-[390px] w-10/12 border border-primary border-opacity-0 rounded-lg overflow-hidden">
-      <ImagePuzzle />
+    <div
+      id="page-container"
+      className="w-full flex flex-col justify-center items-center space-y-5"
+    >
+      <div
+        id="puzzle-container"
+        className="h-[390px] w-10/12 border border-primary border-opacity-0 rounded-lg overflow-hidden"
+      >
+        <ImagePuzzle />
+      </div>
+      <div className="flex w-10/12">
+        <p className="font-semibold">No cheating!!!</p>
+      </div>
     </div>
   );
 };
